45b641055c6bbd8ef532c6a2ccdf4eea04171bb6
nakatsuji <nakatsuji.cfd@gmail.com>
2023-08-17 16:41:49 +0900

    openTK1

---
+++ game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenTK;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Graphics.OpenGL;
+using OpenTK.Mathematics;
+using System.ComponentModel;
+
+namespace BasicOpenTK
+{
+    public class Game : GameWindow
+    {
+        public Game()
+            :base(GameWindowSettings.Default, NativeWindowSettings.Default)// { }
+        { 
+       }
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(new Color4(0.1f, 7.0f, 0.1f, 0.1f));
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            this.Context.SwapBuffers();
+            base.OnRenderFrame(args);
+        }
+
+    }
+}
